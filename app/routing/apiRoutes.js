629895a604ff0dbe2fe8e384620af48a575b8bd0
Skyler La <yenla@Skylers-MacBook-Pro.local>
2017-05-04 18:36:48 -0700

    In proccess of debugging error

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,43 +15,46 @@ module.exports = function(app) {
 	app.post("/api/friends", function(req, res) {
 		console.log('inside post');
 		console.log(typeof(surveyData));
-		console.log(req.body);
+		console.log(surveyData);
+		// console.log(req.body);
 
 		var bestFriendIndex = 0;
     	var minimumDifference = 40;
 
 		// store new friend
-		surveyData.push(req.body);
 
-		function surveyPeople(cb){
+		function surveyPeople(request, cb){
 
+			var body = req.body;
 			var db = [];
 			surveyData.forEach(function(el){
 				var dbObj = {};
 				dbObj.name = el.name;
 				var totalDifference = 0;
-				el.scores.forEach(function(score, idx){
-					var difference = Math.abs(parseInt(score) - parseInt(req.body.questionaires[idx]));
-					totalDifference += difference;
-				});
+				console.log(el.scores);
+				// el.scores.forEach(function(score, idx){
+				// 	var difference = Math.abs(parseInt(score) - parseInt(req.body.questionaires[idx]));
+				// 	totalDifference += difference;
+				// });
 
-				var minimumDifference = dbObj.totalDifference;
-				minimumDifference = 20;
-				console.log(totalDifference);
+				// var minimumDifference = dbObj.totalDifference;
+				// minimumDifference = 40;
+				// console.log(totalDifference);
 
-				if(totalDifference < minimumDifference) {
-					minimumDifference = totalDifference;
-					db.push(dbObj);
-				}
+				// if(totalDifference < minimumDifference) {
+				// 	minimumDifference = totalDifference;
+				// 	db.push(dbObj);
+				// }
 			});
 
 			res.json(surveyData[bestFriendIndex]);
 
-			cb(db);
+			cb(db, body);
 		}
 
-		surveyPeople(function(db){
+		surveyPeople(req, function(db, reqBody){
 			console.log(db);
+			surveyData.push(reqBody);
 			// Sort through the total different to grab the lowest scores.
 
 		})
